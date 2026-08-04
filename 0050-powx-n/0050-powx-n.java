2125af291ab76291ef7c5544dfc3fcb241977290
Valoor007 <mahendersinghjaat007@gmail.com>
2026-08-04 23:10:17 +0530

    Time: 0 ms (100%), Space: 48.4 MB (11.54%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public double power(double x, long n) {
+        if (n == 0) return 1.0;
+
+                   if (n % 2 == 0) {
+            return power(x * x, n / 2);
+        }
+
+               return x * power(x, n - 1);
+    }
+    public double myPow(double x, int n) {
+        long N = n;
+                if (N < 0) {
+            return 1.0 / power(x, -N);
+        }
+return power(x, N);
+    }
+}
